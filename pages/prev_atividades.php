2b070900f18dc9b2194834b7d579f6207adc1552
bagil01 <bagil2203@gmail.com>
2025-07-09 14:36:21 -0300

    titulo do index

--- pages/prev_atividades.php
+++ pages/prev_atividades.php
@@ -16,6 +16,7 @@ $resultado = $stmt->get_result();
 
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Atividades</title>
     <link rel="stylesheet" href="../assets/css/prev_atividades.css">
     <link rel="stylesheet" href="../assets/icons/fontawesome-free-6.5.2-web/css/all.css">
